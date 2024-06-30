b460a78fd2a8edf6cfd41aee3a97ce7f13c76beb
Muhammad Sya'bani Falif <msfalif404@gmail.com>
2024-06-30 23:06:46 +0700

    Add records

--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from .forms import SignUpForm
+from .models import Record
+
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if user already logged in
     if request.method == "POST":
         username = request.POST['username']
@@ -17,13 +23,45 @@ def home(request):
             return redirect('home')
         else:
             messages.error(request, "There was an error")
-            
+
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out")
+
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully register")
+
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def user_info(request):
+    user = request.user
+    print(f"Username: {user.username}")
+    print(f"Email: {user.email}")
+
+    return render(request, 'user_info.html', {'user': user})
